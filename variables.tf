3fba052275fe260f48672a664a3e44fdfcdc1bf8
Chaitanya Kolla <chaitanya.kolla@oracle.com>
2022-03-17 19:58:08 +0530

    Update with region

--- variables.tf
+++ variables.tf
@@ -9,5 +9,5 @@ variable tenancy_ocid {
 }
 
 variable compartment_ocid {
-  default = "ocid1.tenancy.oc1..aaaaaaaackopa27emaz4uteg4sseutmk3qq73iyoymttpyjdu34bilvxo3da"
+  default = "ocid1.compartment.oc1..aaaaaaaas5a4ed5wdptp7qqprg7uzxolkcjmlj2agz2mmmzxbsse3vds6sja"
 }
